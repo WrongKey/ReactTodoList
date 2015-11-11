af65cf96ff97e510186753ebf323b0680fadf85c
Kai Dev Hu <kdhu@thoughtworks.com>
2015-11-11 17:05:00 +0800

    add require.

--- src/js/components/filterableTodoList.jsx
+++ src/js/components/filterableTodoList.jsx
@@ -1,6 +1,10 @@
 var AppActions = require('src/js/actions/appActions')
 var ReactDOM = require('react-dom');
 var React = require('react');
+var FilterBox = require('src/js/components/filterBox');
+var TodoList = require('src/js/components/todoList');
+var AddForm = require('src/js/components/addForm');
+
 
 var FilterableTodoList = React.createClass({
     render: function () {
